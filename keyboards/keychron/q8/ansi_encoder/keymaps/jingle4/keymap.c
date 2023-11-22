4e0ff187c3878ba65162b7d1b9129110a6fee673
Jeremy Ingle <jingle@cspire.com>
2023-11-22 11:05:56 -0600

    Added personal profile for Keychron Q8

---
+++ keyboards/keychron/q8/ansi_encoder/keymaps/jingle4/keymap.c
@@ -0,0 +1,77 @@
+/* Copyright 2021 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+// clang-format off
+
+enum layers{
+    MAC_BASE,
+    WIN_BASE,
+    _FN1,
+    _FN2,
+    _FN3
+};
+
+#define KC_TASK LGUI(KC_TAB)
+#define KC_FLXP LGUI(KC_E)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [MAC_BASE] = LAYOUT_ansi_69(
+        QK_GESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
+        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_HOME,
+        KC_CAPS, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,           KC_END,
+        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_RCTL,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
+        KC_LCMD, MO(_FN1),  KC_LCTL,           KC_SPC,           KC_LOPT, KC_RGUI,         KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
+
+    [WIN_BASE] = LAYOUT_ansi_69(
+        QK_GESC,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
+        KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_HOME,
+        KC_CAPS, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,           KC_END,
+        KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_RCTL,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
+        KC_LCTL, MO(_FN1),  KC_LALT,           KC_SPC,           KC_LGUI, KC_RGUI,         KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
+
+    [_FN1] = LAYOUT_ansi_69(
+        KC_GRV,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,  KC_F11,  KC_F12,  KC_DEL,          RGB_TOG,
+        _______, RGB_HUD,  RGB_HUI,  RGB_SAD, RGB_SAI, _______, _______,  _______, KC_UP, _______, _______,  _______,  _______,  _______,          _______,
+        _______, RGB_VAD, RGB_VAI,  RGB_SPD, RGB_SPI, _______,           KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,  _______,  _______,  _______,          _______,
+        _______,           RGB_TOG,  RGB_RMOD, RGB_MOD, RGB_M_P, _______,  _______, KC_END, _______, _______,  _______,  _______,  _______, _______,
+        _______, _______,  KC_MCTL,           MO(_FN2),          KC_MCTL,  _______,          _______,           _______,            _______, _______, _______),
+
+    [_FN2] = LAYOUT_ansi_69(
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK, KC_FLXP, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          RGB_TOG,
+        _______, _______,  _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          QK_BOOT,
+        _______, _______,  _______,  _______, _______, _______,           _______, _______, _______, _______,  _______,  _______,  _______,          NK_TOGG,
+        _______,           _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______, _______,
+        _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______),
+
+    [_FN3] = LAYOUT_ansi_69(
+        _______, _______,    _______,   _______,  _______,  _______,   _______,   _______,   _______,  _______,   _______,   _______,   _______,   _______,          _______,
+        _______, _______,  _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          _______,
+        _______, _______, _______,  _______, _______, _______,           _______, _______, _______, _______,  _______,  _______,  _______,          _______,
+        _______,           _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______, _______,
+        _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______)
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [_FN1]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
+    [_FN2]   = {ENCODER_CCW_CW(RGB_SPD, RGB_SPI)},
+    [_FN3]   = {ENCODER_CCW_CW(_______, _______)}
+};
+#endif // ENCODER_MAP_ENABLE
